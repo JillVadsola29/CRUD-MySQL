a28e74fa8e712f93bfe05036009b7976dc65b300
Jill-Vadsola <jvadsola@student.concordia.ab.ca>
2024-12-02 13:59:08 -0700

    Changed name

--- ecommers-ui/src/App.js
+++ ecommers-ui/src/App.js
@@ -11,7 +11,7 @@ function App() {
         <AppBar position="static">
           <Toolbar>
             <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-              My App
+              Crud-MySQL
             </Typography>
             <Button color="inherit" component={Link} to="/users">
               Users
